a23e85620a404b32b5aecae85c086e8f96595723
hclee <hclee@muhayu.com>
2017-06-12 20:56:24 +0900

    Initial commit

---
+++ chapter07/src/main/java/com/muhayu/Chapter07Application.java
@@ -0,0 +1,12 @@
+package com.muhayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter07Application.class, args);
+	}
+}
