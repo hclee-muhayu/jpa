2a13b8902f39645105696f2ff51ce63a1ed6f78b
hycheon <rainbow880616@gamil.com>
2017-06-13 23:41:48 +0900

    11장 끝

--- chapter11/src/main/java/com/muhayu/controller/ItemController.java
+++ chapter11/src/main/java/com/muhayu/controller/ItemController.java
@@ -1,11 +1,14 @@
 package com.muhayu.controller;
 
 import com.muhayu.domain.item.Book;
+import com.muhayu.domain.item.Item;
 import com.muhayu.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by hyecheon on 2017. 6. 13..
@@ -15,13 +18,33 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        final List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "/items/itemList";
+    }
+
+    @GetMapping(value = "/items/{id}/edit")
+    public String updateItemForm(@PathVariable Long id, Model model) {
+        final Item item = itemService.findOne(id);
+        model.addAttribute(item);
+        return "/item/updateItemForm";
+    }
+
+    @PostMapping(value = "/items/{id}/edit")
+    public String updateItem(@ModelAttribute("item") Book item) {
+        itemService.saveItem(item);
+        return "redirect:/items";
+    }
+
     @RequestMapping(value = "/items/new", method = RequestMethod.GET)
-    public String createForm() {
-        return "items/createItemForm";
+    public String createForm(@ModelAttribute("item") Book item) {
+        return "/items/createItemForm";
     }
 
     @RequestMapping(value = "/items/new", method = RequestMethod.POST)
-    public String create(Book item) {
+    public String create(@ModelAttribute("item") Book item) {
         itemService.saveItem(item);
         return "redirect:/items";
     }
